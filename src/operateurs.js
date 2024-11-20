80d203a998dc0ce7319e63c507bcc255e831a920
Florian <floriangeorges1992@gmail.com>
2024-11-20 11:22:30 +0100

    add course in .txt abd operateur.js
    Tableau de précedence

---
+++ src/operateurs.js
@@ -0,0 +1,11 @@
+// Operateurs
+//La précedence des opérateurs est déterminée par la table des précédences
+//donc d'abors la priorité du nombre de la catégorie et ensuite l'associativité si il y a une égalité
+//entre la catégorie des opérateurs
+
+let a = ((2 + 2) * 6) / 2;
+let b = 1;
+b += 2;
+b += 5;
+console.log(b);
+console.log(a);
